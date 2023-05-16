1d20b637b295cb1c75152c64b58371c872eb02ba
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-05-16 16:27:15 -0300

    [update] CORS cofing

--- api/index.js
+++ api/index.js
@@ -5,13 +5,9 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log('My port:', port)
-})
-
 app.use(express.json())
 
-const whiteList = ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://shop-clothify.web.app/']
+const whiteList = ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://shop-clothify.web.app']
 const options = {
   origin: (origin, callback) => {
     if (whiteList.includes(origin) || !origin) {
@@ -22,10 +18,15 @@ const options = {
   }
 }
 
+app.use(cors(options))
+
 app.get('/api', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173')
   res.send('Welcome to Clothify API. Go to /products')
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
 })
 
-app.use(cors(options))
 routerApi(app)
+
+app.listen(port, () => {
+  console.log('My port:', port)
+})
